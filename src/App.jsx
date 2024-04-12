6c7835b9b1f2ee4655be69b69c037484f95d37ca
Hamilton <hpipicano@unicauca.edu.co>
2024-04-12 18:40:11 -0500

    primera prueba conexion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import UserConsult from "./Pages/UserConsult";
 import TablaConsulta from "./Pages/TablaConsulta";
@@ -10,11 +10,11 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/consultar" element={<UserConsult />} />
-        </Routes>
-        <Routes>
           <Route path="/consultar/consulta" element={<TablaConsulta />} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
         </Routes>
       </Router>
     </>
--- src/Components/Layout/ResponsiveTable.jsx
+++ src/Components/Layout/ResponsiveTable.jsx
@@ -1,44 +1,64 @@
-import Table from "react-bootstrap/Table";
+import React, { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
+import axios from 'axios';
 
 function ResponsiveTable() {
-  return (
-    <Table responsive bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <th key={index}>Table heading</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>2</td>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>3</td>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>4</td>
-          {Array.from({ length: 10 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-      </tbody>
-    </Table>
-  );
+    const [movimientos, setMovimientos] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/v1/posgrados/report/')
+            .then(response => {
+                setMovimientos(response.data);
+                console.log("respuesta :", movimientos);
+            })
+            .catch(error => {
+                console.error("Error fetching data: ", error);
+                setMovimientos([]);
+            });
+
+            console.log("respuesta :", movimientos);
+    }, []);
+
+    return (
+        <Table responsive bordered hover>
+            <thead>
+                <tr>
+                    <th>N°</th>
+                    <th>Tipo Documento</th>
+                    <th>Número Movimiento</th>
+                    <th>Fecha</th>
+                    <th>Cuenta Movimiento</th>
+                    <th>Observación</th>
+                    <th>Valor Definitivo</th>
+                    <th>Valor Registro</th>
+                    <th>Valor Ejecutado</th>
+                    <th>Valor Pagado</th>
+                    <th>Saldo</th>
+                    <th>Estado</th>
+                    <th>ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                {movimientos.map((movimiento, index) => (
+                    <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{movimiento.tipo_documento}</td>
+                        <td>{movimiento.numero_movimiento}</td>
+                        <td>{movimiento.fecha}</td>
+                        <td>{movimiento.cuenta_movimiento}</td>
+                        <td>{movimiento.observacion}</td>
+                        <td>{movimiento.valor_definitivo}</td>
+                        <td>{movimiento.valor_registro}</td>
+                        <td>{movimiento.valor_ejecutado}</td>
+                        <td>{movimiento.valor_pagado}</td>
+                        <td>{movimiento.saldo}</td>
+                        <td>{movimiento.estado}</td>
+                        <td>{movimiento.id}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    );
 }
 
 export default ResponsiveTable;
