7f462d4561c9b86bec278d0c79c2aae9811f7106
Hamilton <hpipicano@unicauca.edu.co>
2024-05-06 15:12:44 -0500

    conexion ingresos y gastos

--- src/Components/UI/TextInput.jsx
+++ src/Components/UI/TextInput.jsx
@@ -2,7 +2,7 @@ import { useThemeContext } from "../../ThemeContext";
 import PropTypes from "prop-types";
 import "../../Styles/Components/TextInput.css";
 
-const TextInput = ({ label, placeholder }) => {
+const TextInput = ({ label, placeholder, onChange  }) => {
   const { contextTheme } = useThemeContext();
 
   return (
@@ -18,14 +18,16 @@ const TextInput = ({ label, placeholder }) => {
         type="text"
         className={`textInput ${contextTheme === "Dark" ? "dark-input" : ""}`}
         placeholder={placeholder}
+        onChange={onChange} // Propagamos el evento onChange al input
       />
     </div>
   );
 };
-
+// Define PropTypes para asegurarte de que se pasen las props correctamente
 TextInput.propTypes = {
   label: PropTypes.string.isRequired,
   placeholder: PropTypes.string.isRequired,
+  onChange: PropTypes.func, // Indicamos que onChange es opcional
 };
 
 export default TextInput;
